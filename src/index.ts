69427e08ddcb6d0b93e0cd24e935c3fb2545a4ca
Gabriel Zambrano <gab.zambrano@gmail.com>
2025-04-19 14:08:34 +0200

    localprovider support

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 // src/index.ts
 export { CourseCatalogue } from './react/CourseCatalogue';
 export { CourseCatalogueCore } from './core/services';
-export type { CatalogueConfig, GeoHit } from './core/types';
+export type { CatalogueConfig, GeoHit, TypesenseConfig } from './core/types';
 export { default as App } from './demo/App';
+
+// Add these exports to make subpath imports unnecessary
+export { LocaleProvider } from './i18n/LocalContext';
+export type { Locale } from './i18n/translations';
